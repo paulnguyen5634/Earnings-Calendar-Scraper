f399fb2ed6754dcbfd6ba2ebe82059a50313d929
paulnguyen5634 <paulnguyen@ufl.edu>
2024-01-07 22:03:09 -0500

    Added source handling function

--- NASDAQ_Scraper.py
+++ NASDAQ_Scraper.py
@@ -8,11 +8,8 @@ from pprint import pprint
 # Import date class from datetime module
 from datetime import date
 
-def main():
-    '''
-    Scrapes the NASDAQ earnings calender for earnings releases coming TODAY. 
-    Will not work on holidays/weekends
-    '''
+def getSource():
+
     # Returns the current local date
     today = date.today()
     print("Today date is: ", today)
@@ -29,6 +26,49 @@ def main():
         day = str(day)
         day = ''.join(('0',day))
 
+    headers = {
+        "Accept":"application/json, text/plain, */*",
+        "Accept-Encoding":"gzip, deflate, br",
+        "Accept-Language":"en-US,en;q=0.9",
+        "Origin":"https://www.nasdaq.com",
+        "Referer":"https://www.nasdaq.com",
+        "User-Agent":"your user agent..."
+    }
+ 
+    url = 'https://api.nasdaq.com/api/calendar/earnings?' 
+    #Change this according to day
+    payload = {"date":f"{year}-{month}-{day}"} 
+    source = requests.get( url=url, headers=headers, params=payload, verify=True ) 
+    data = source.json()
+
+    return data
+
+def main():
+    '''
+    Scrapes the NASDAQ earnings calender for earnings releases coming TODAY. 
+    Will not work on holidays/weekends
+    '''
+    
+    data = getSource()
+    for i in range(0,len(data['data']['rows'])):
+    
+        mrkt_cap = data['data']['rows'][i]['marketCap']
+        mrkt_cap = mrkt_cap.replace('$', '')
+        mrkt_cap = mrkt_cap.replace(',', '')
+        
+        forecastEPS = data['data']['rows'][i]['epsForecast']
+        
+        numAnalysts = data['data']['rows'][i]['noOfEsts']
+        
+        releaseTime = data['data']['rows'][i]['time'] 
+        
+        #Only showing companies with abova 1billion$ mrktCap
+        if int(mrkt_cap)/100000000 > 1 and releaseTime == 'time-pre-market':
+            print(data['data']['rows'][i]['symbol'])
+            print(releaseTime)
+            print(data['data']['rows'][i]['marketCap'])
+            print('Forcasted EPS: '+forecastEPS)
+            print('Num analysts: ' + numAnalysts + "\n")
     return
 
 if __name__ == '__main__':
