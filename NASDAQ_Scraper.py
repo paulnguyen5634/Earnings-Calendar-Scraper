2ddaae5a92911ac90352503f384559a5c2a23361
paulnguyen5634 <paulnguyen@ufl.edu>
2024-01-07 21:50:20 -0500

    Started base for NASDAQ SCRAPING

---
+++ NASDAQ_Scraper.py
@@ -0,0 +1,9 @@
+import pandas as pd # library for data analysis
+import numpy as np
+import requests # library to handle requests
+from bs4 import BeautifulSoup # library to parse HTML documents
+from selenium import webdriver
+import time
+from pprint import pprint
+# Import date class from datetime module
+from datetime import date
