300885018e75a7e1bb791c88260a9ebcfd6572d6
paulnguyen5634 <paulnguyen@ufl.edu>
2024-01-07 22:00:00 -0500

    Added days grabbing code

--- NASDAQ_Scraper.py
+++ NASDAQ_Scraper.py
@@ -7,3 +7,29 @@ import time
 from pprint import pprint
 # Import date class from datetime module
 from datetime import date
+
+def main():
+    '''
+    Scrapes the NASDAQ earnings calender for earnings releases coming TODAY. 
+    Will not work on holidays/weekends
+    '''
+    # Returns the current local date
+    today = date.today()
+    print("Today date is: ", today)
+
+    year = today.year
+
+    month = today.month
+    if month < 10:
+        month = str(month)
+        month = ''.join(('0',month))
+        
+    day = today.day
+    if day < 10:
+        day = str(day)
+        day = ''.join(('0',day))
+
+    return
+
+if __name__ == '__main__':
+    main()
